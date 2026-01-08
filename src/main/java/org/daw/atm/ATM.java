560d93c00a6a7b6d623d9609a6e2963c7c8690c0
Joan Pomés <jopo738@vidalibarraquer.net>
2026-01-08 10:57:00 +0100

    avanç del dia 08/01/2026

--- src/main/java/org/daw/atm/ATM.java
+++ src/main/java/org/daw/atm/ATM.java
@@ -11,6 +11,43 @@ package org.daw.atm;
 public class ATM {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+       
+        //System.out.println(Compte.comptador);
+        //Compte.comptador++;
+        //System.out.println(Compte.comptador);
+
+        
+
+        Compte c1 = new Compte();
+        //c1.numero = "EI!!11111";
+        //c1.setNumero("EI!!11111");
+        System.out.println(c1.getNumero());
+        c1.setSaldo(100);
+        c1.setInteresAnual(10);
+        //c1.saldo=100;
+
+        c1.ingressar(100);
+        c1.retirar(50);
+        c1.calculaInteresAnual();
+        System.out.println(c1.getSaldo());
+        
+
+        Compte c2 = new Compte();
+        System.out.println(c2.getNumero());
+        //c2.numero="EI!!22222";
+        //c2.saldo=200;
+        //c2.setNumero("EI!!22222");
+        c2.setSaldo(200);
+
+        Compte c3;
+        c3=c1;
+        //c3.saldo=0;
+
+        Compte c4 = new Compte(300); // amb una sola linia podem donar el valor gracies al constructor que hem fet
+    System.out.println(c4.getNumero());
+
+    c1.transfarencia(c2, 10);
+    System.out.println("saldo del compte 2: "+ c2.getSaldo());
+    System.out.println("saldo del compte 1: "+c1.getSaldo());
     }
 }
---
+++ src/main/java/org/daw/atm/Compte.java
@@ -0,0 +1,101 @@
+package org.daw.atm;
+
+public class Compte {
+    
+    // Atributs    // si no hi ha res escrit es "public"
+    private String numero;
+    private double saldo;
+    private String titular;
+    private double interesAnual;
+    
+    // perque sigui un atribut que comparteixen tots els objectes que siguin de la classe ("compte" en aquest cas) hem de posar static
+    private static int comptador=1;
+    private static final String prefix="ES100-1111-"; // es posa el final perque aquest valor que li he posat inicialment ja no es pugui canviar i sempre tingui el mateix valor
+
+
+    // el constructor s'ha de dir igual que el public class
+    public Compte() {
+        //numero = "ES100-1111-"+comptador; //per convertir un numero  a un string posem el (""+)
+        numero = prefix+comptador;   // en aquest cas el "" es substitueix pel prefix definit previament
+        comptador++;
+        //System.out.println("Compte creat");
+    }
+
+    
+
+    public Compte( double saldo) {
+        //this.numero = numero;
+        numero = prefix+comptador; //per convertir un numero  a un string posem el (""+)
+        comptador++;
+        this.saldo = saldo;
+    }
+
+
+
+    // podem treure el get i el set perque siguin mes privats
+    public String getNumero() {
+        return numero;
+    }
+    public void setNumero(String numero) {   //el set dona valor atribut
+        this.numero = numero;     // el "numero" del this fa referència al d'adalt i el que hi ha despres del "=" fa referencia al del "String"
+    }
+    public double getSaldo() {
+        return saldo;
+    }
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+    public String getTitular() {
+        return titular;
+    }
+    public void setTitular(String titular) {
+        this.titular = titular;
+    }
+    public double getInteresAnual() {
+        return interesAnual;
+    }
+    public void setInteresAnual(double interesAnual) {
+        this.interesAnual = interesAnual;
+    }
+
+    public double calculaInteresAnual(){
+     //double interes= ((saldo*interesAnual)/100);
+     //saldo=interes;
+
+     this.saldo= this.saldo +this.interesAnual/100*this.saldo;
+     return this.saldo;
+    }
+
+    
+
+    public void ingressar (double quantitat){
+     if(quantitat>0){
+        saldo=saldo+quantitat;
+    }
+     
+    }
+
+    public boolean retirar (double quantitat){ // posem un boolean per poder dir si la retirada es pot fer o no es pot fer
+     if(saldo>=quantitat && quantitat>0){
+        saldo=saldo-quantitat;
+        return true;
+     }
+     return false;
+     
+    }
+    public void transfarencia (Compte c,double quantitat){
+        if(saldo>=quantitat && quantitat>0){
+            this.retirar(quantitat);
+            c.ingressar(quantitat);
+        }
+    }
+
+
+    
+
+
+
+    
+
+    
+}
