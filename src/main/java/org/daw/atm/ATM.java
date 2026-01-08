ade6a3c0b196d1a53f14ee76d4f1f7f5edfb3ae5
Joan Pomés <jopo738@vidalibarraquer.net>
2026-01-08 08:17:25 +0100

    first commit 8/1/2026

---
+++ src/main/java/org/daw/atm/ATM.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package org.daw.atm;
+
+/**
+ *
+ * @author jpome
+ */
+public class ATM {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
